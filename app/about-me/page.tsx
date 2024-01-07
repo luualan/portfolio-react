3e55afbe40c97e0e8f392eebad73790ef7f22624
Alan Luu <59902126+alanviluu4@users.noreply.github.com>
2024-01-06 17:04:29 -0800

    first commit

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -5,13 +5,6 @@ import Image from "next/image";
 const Page = () => {
   return (
     <div className="bg-no-repeat bg-cover overflow-hidden">
-      {/* <Image
-        src="/assets/top-left-img.png"
-        alt="paint"
-        width={230}
-        height={230}
-        className="absolute left-0 top-0"
-      /> */}
       <Profile />
     </div>
   );
